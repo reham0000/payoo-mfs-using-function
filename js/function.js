7cfe55663700f4ca8220ced668c7c4b1d80c88e9
reham0000 <amsarkerreham@gmail.com>
2024-09-20 16:17:20 +0600

    activate all buttons

--- js/function.js
+++ js/function.js
@@ -16,3 +16,11 @@ function getTextFieldValueById(id){
     const textNumber = parseFloat(textValue);
     return textNumber;
 }
+
+function showSectionById(id){
+    document.getElementById('add-money-form').classList.add('hidden');
+    document.getElementById('cash-out-form').classList.add('hidden');
+    document.getElementById('transaction-section').classList.add('hidden');
+
+    document.getElementById(id).classList.remove('hidden');
+}   
