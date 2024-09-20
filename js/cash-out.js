3aa9e2e92d01ab3c8a5c2622a29b022e690dc3e2
reham0000 <amsarkerreham@gmail.com>
2024-09-20 15:43:26 +0600

    cash out system added

--- js/cash-out.js
+++ js/cash-out.js
@@ -2,5 +2,14 @@ document.getElementById('btn-cash-out').addEventListener('click', function(event
     event.preventDefault();
     const cashOut = getInputFieldValueById('input-cash-out');
     const pinNumber = getInputFieldValueById('input-cash-out-pin');
-    console.log(cashOut, pinNumber);
+    
+    
+    if(pinNumber === 1234) {
+        const balance = getTextFieldValueById('account-balance');
+        const newBalance = balance - cashOut;
+        document.getElementById('account-balance').innerText = newBalance;
+    }
+    else{
+        alert('Please Try again later');
+    }
 });
