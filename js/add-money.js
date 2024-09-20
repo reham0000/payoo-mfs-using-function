8774624e8a6032abd86521293fd7775e542540bb
reham0000 <amsarkerreham@gmail.com>
2024-09-20 17:02:28 +0600

    cash out transaction and add money transaction history added

--- js/add-money.js
+++ js/add-money.js
@@ -14,6 +14,12 @@ document.getElementById('btn-add-money')
             const balance = getTextFieldValueById('account-balance');
             const newBalance = balance + addMoney;
             document.getElementById('account-balance').innerText = newBalance;
+
+            const p = document.createElement('p');
+            p.innerText = `Added: ${addMoney} TK. Balance: ${newBalance}`;
+            console.log(p);
+
+            document.getElementById('transaction-container').appendChild(p);
         }
         else{
             alert('Please Try again');
--- js/cash-out.js
+++ js/cash-out.js
@@ -8,6 +8,15 @@ document.getElementById('btn-cash-out').addEventListener('click', function(event
         const balance = getTextFieldValueById('account-balance');
         const newBalance = balance - cashOut;
         document.getElementById('account-balance').innerText = newBalance;
+
+        const div = document.createElement('div');
+        div.classList.add('bg-yellow-300');
+        div.innerHTML = `
+            <h4 class="text-2xl font-bold">Cash Out</h4>
+            <p>${cashOut} Withdraw. New Balance ${newBalance}</p>
+
+        `
+        document.getElementById('transaction-container').appendChild(div);  
     }
     else{
         alert('Please Try again later');
